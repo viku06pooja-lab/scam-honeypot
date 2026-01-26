0d746586ee20c3a41d64aa4973d21028999d3b4e
viku06pooja-lab <viku06pooja@gmail.com>
2026-01-26 02:06:52 -0800

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,7 +2,17 @@ from fastapi import FastAPI, Header, HTTPException
 from pydantic import BaseModel
 
 app = FastAPI()
+from fastapi.middleware.cors import CORSMiddleware
 
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 @app.get("/")
 def root():
     return {"status": "ok", "message": "Scam Honeypot API is running"}
